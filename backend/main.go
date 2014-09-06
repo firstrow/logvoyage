b873d3ace8539ffa6e32a0cb12285cfd267622b3
firstrow <firstrow@gmail.com>
2014-09-06 14:26:39 +0300

    Added command line args to backend

--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,11 @@
+// Backend server - main part of LogVoyage service.
+// It acceptc connections from "Client", parses string and pushes it to ElasticSearch index
 package main
 
 import (
 	"bytes"
 	"encoding/json"
+	"flag"
 	"github.com/firstrow/logvoyage/common"
 	"github.com/firstrow/logvoyage/tcp_server"
 	"io/ioutil"
@@ -10,6 +13,40 @@ import (
 	"net/http"
 	"strings"
 )
+
+var (
+	defaultHost = ""
+	defaultPort = "27077"
+)
+
+func main() {
+	log.Print("Initializing server")
+
+	host := flag.String("host", defaultHost, "Host to open server. Set to `localhost` to accept only local connections.")
+	port := flag.String("port", defaultPort, "Port to accept new connecetions. Default value: "+defaultPort)
+	flag.Parse()
+
+	server := tcp_server.NewServer(*host + ":" + *port)
+	server.OnNewClient(func(c *tcp_server.Client) {
+		log.Print("New client")
+	})
+	server.OnNewMessage(func(c *tcp_server.Client, message string) {
+		message = strings.TrimSpace(message)
+		// Send data to elastic
+		record := &common.LogRecord{
+			Message: message,
+		}
+		json, err := json.Marshal(record)
+		if err != nil {
+			log.Print("Error encoding message")
+		}
+		sendToElastic(string(json))
+	})
+	server.OnClientConnectionClosed(func(c *tcp_server.Client, err error) {
+		log.Print("Client disconnected")
+	})
+	server.Listen()
+}
 
 func sendToElastic(json string) {
 	url := "http://localhost:9200/firstrow/logs"
@@ -32,28 +69,4 @@ func sendToElastic(json string) {
 	// If dont read body golang will keep connection open
 	ioutil.ReadAll(resp.Body)
 	log.Print("Message sent to Elastic: " + json)
-}
-
-func main() {
-	log.Print("Initializing server")
-	server := tcp_server.NewServer(":9999")
-	server.OnNewClient(func(c *tcp_server.Client) {
-		log.Print("New client")
-	})
-	server.OnNewMessage(func(c *tcp_server.Client, message string) {
-		message = strings.TrimSpace(message)
-		// Send data to elastic
-		record := &common.LogRecord{
-			Message: message,
-		}
-		json, err := json.Marshal(record)
-		if err != nil {
-			log.Print("Error encoding message")
-		}
-		sendToElastic(string(json))
-	})
-	server.OnClientConnectionClosed(func(c *tcp_server.Client, err error) {
-		log.Print("Client disconnected")
-	})
-	server.Listen()
 }
