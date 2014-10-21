9f01709b5a198ac61c6815e3e93c910548f719c8
firstrow <firstrow@gmail.com>
2014-10-22 00:54:43 +0300

    Added sample template

--- web/server/server.go
+++ web/server/server.go
@@ -1,11 +1,16 @@
 package main
 
-import "github.com/go-martini/martini"
+import (
+	"github.com/codegangsta/martini-contrib/render"
+	"github.com/go-martini/martini"
+)
 
 func main() {
 	m := martini.Classic()
-	m.Get("/", func() string {
-		return "Hello world!"
+	m.Use(render.Renderer())
+
+	m.Get("/", func(r render.Render) {
+		r.HTML(200, "index", "world")
 	})
 	m.Run()
 }
