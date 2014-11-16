cb89ad6d1e7165949404cb778f8b83329101a126
firstrow <firstrow@gmail.com>
2014-11-16 22:02:37 +0200

    Renamed modal

--- static/js/view.js
+++ static/js/view.js
@@ -1,13 +1,13 @@
 // Helps to view log records
 $(function() {
-	$(".view").click(function(event) {
+	$("a.view").click(function(event) {
 		event.preventDefault();
 		$.getJSON($(this).attr("href"), function(data) {
 			$(".modal-body").JSONView(data);
-			$("#myModal").modal();
+			$("#viewRecordModal").modal();
 		}).fail(function() {
 			$(".modal-body").html("Error: Record not found or wrong JSON structure.");
-			$("#myModal").modal();
+			$("#viewRecordModal").modal();
 		});
 	});
 });
