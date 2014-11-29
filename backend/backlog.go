21bb93f49d8b64e6dc177fb64d3e516a25f89053
firstrow <firstrow@gmail.com>
2014-11-29 15:30:00 +0200

    Text message

--- backend/backlog.go
+++ backend/backlog.go
@@ -8,7 +8,7 @@ import (
 func toBacklog(message string) {
 	path, err := os.Getwd()
 	if err != nil {
-		log.Println("Can not define current directory")
+		log.Println("Can't define current directory")
 	}
 	fullPath := path + string(os.PathSeparator) + "back.log"
 	file, err := os.OpenFile(fullPath, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0660)
@@ -18,4 +18,8 @@ func toBacklog(message string) {
 	}
 	defer file.Close()
 	file.Write([]byte(message))
+}
+
+func resend() {
+
 }
